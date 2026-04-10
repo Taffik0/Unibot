935c19824110e91ff6fad607a1b83605901daf21
Taffik <Taff1k.tpl@gmai.com>
2026-04-10 23:38:45 +0300

    warn, error, debug method in logger

--- unibot/logger/logger.py
+++ unibot/logger/logger.py
@@ -4,11 +4,26 @@ RESET: str = "\033[0m"
 
 
 class Logger():
-    def __init__(self, info_color: str = "\033[1;32m"):
-        self.info_color = info_color
+    def __init__(self, info_color: str = "\033[1;32m",
+                 warn_color: str = "\033[1;33",
+                 debug_color: str = "\033[1;34",
+                 error_color: str = "\033[1;31"):
+        self._info_color = info_color
+        self._warn_color = warn_color
+        self._debug_color = debug_color
+        self._error_color = error_color
 
     def info(self, msg: str):
-        print(f"{datetime.now()} {self.info_color}[INFO]{RESET} {msg}")
+        print(f"{datetime.now()} {self._info_color}[INFO]{RESET} {msg}")
+
+    def warn(self, msg: str):
+        print(f"{datetime.now()} {self._warn_color}[WARN]{RESET} {msg}")
+
+    def debug(self, msg: str):
+        print(f"{datetime.now()} {self._debug_color}[DEBUG]{RESET} {msg}")
+
+    def error(self, msg: str):
+        print(f"{datetime.now()} {self._error_color}[ERROR]{RESET} {msg}")
 
 
 _logger: Logger | None = None
